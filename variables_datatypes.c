27bab5e93ba474fe9e8056dd9dac05df0b5e2e43
k256505 <k256505@nu.edu.pk>
2026-02-07 17:19:47 +0000

    Create variable_datatypes.c

---
+++ variables_datatypes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+    int num = 125;
+    char alpha = 'A';
+    float decimal = 0.125;
+    bool decision = true;
+    double bigDecimal = 0;
+    printf("Interger: %d\n",num);
+    printf("Character: %c\n",alpha);
+    printf("Float: %f\n",decimal);
+    printf("Boolean: %d\n",decision);
+    printf("Double: %lf\n",bigDecimal);    
+}
